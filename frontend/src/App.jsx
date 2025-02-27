5d96d1b07878c9066058e9c88a3cb0c33c6753ba
Sieu Nguyen Thanh <122494878+tsdevtool@users.noreply.github.com>
2025-02-27 11:04:50 +0700

    Done connect mongdb with laravel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 
 import NotFound from "./pages/404/NotFound";
@@ -6,16 +6,37 @@ import NotFound from "./pages/404/NotFound";
 import AuthPage from "./pages/auth/AuthPage";
 import ForgotPasswordPage from "./pages/auth/ForgotPasswordPage";
 import Footer from "./components/Footer";
+import { Toaster } from "react-hot-toast";
+import { useAuthStore } from "./store/authUser";
+import { useEffect } from "react";
+import { Loader } from "lucide-react";
 function App() {
+  const { user, isCheckingAuth, authCheck } = useAuthStore();
+  useEffect(() => {
+    authCheck();
+  }, [authCheck]);
+  if (isCheckingAuth) {
+    return (
+      <div className="h-screen">
+        <div className="flex justify-center items-start bg-black h-full">
+          <Loader className="animate-spin text-cyan-600 size-10" />
+        </div>
+      </div>
+    );
+  }
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/auth" element={<AuthPage />} />
+        <Route
+          path="/auth"
+          element={!user ? <AuthPage /> : <Navigate to="/" />}
+        />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
       <Footer />
+      <Toaster />
     </>
   );
 }
